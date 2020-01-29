a783f40af43f3323531249405aa2992b45ec187d
sanhihi <59355879+sanhihi@users.noreply.github.com>
2020-01-30 02:43:58 +0900

    #2.7 DOM - If else - Function practice part Two

--- js/index.js
+++ js/index.js
@@ -1,32 +1,12 @@
-const aa = document.querySelector("#a").style;
-const qq = document.querySelector("#a");
+const aa = document.querySelector("#a");
 
-const bb = "red";
-const cc = "blue";
+const b = "plpl";
 
-function b() {
-  const d = aa.color;
-  if (d === bb) {
-    aa.color = cc;
-  } else {
-    aa.color = bb;
-  }
+function bb() {
+  aa.classList.toggle(b);
 }
 
 function c() {
-  aa.color = bb;
-  qq.addEventListener("mouseenter", b);
+  aa.addEventListener("click", bb);
 }
 c();
-
-function zz() {
-  console.log("I'm out");
-}
-
-window.addEventListener("offline", zz);
-
-function pp() {
-  console.log("I'm in");
-}
-
-window.addEventListener("online", pp);
